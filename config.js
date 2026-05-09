e8f2b298cec056cf30c4989abaac54082e3b85e7
zyweimian <18786663617@163.com>
2026-05-10 00:01:08 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://djxhsjcloaetuxlmenmh.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqeGhzamNsb2FldHV4bG1lbm1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjk5MzgsImV4cCI6MjA5MzkwNTkzOH0.FBEB0lFaEDxQgqA8NWikmY0dVz7wr5pOyaRjql1CJ6w",
-  functionsBaseUrl: ""
+  functionsBaseUrl: ""   // 关键：必须是空字符串
 };
